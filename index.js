3a7cd1d83cd20b6ebaeb61aefcdb4186e0e04e82
tellapurivenkatesh123 <tellapurivenkatesh6635@gmail.com>
2026-01-14 00:31:08 +0530

    mern

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-const v = require('lodash');
-const numbers = [1, 2, 3, 4, 5];
-const sum= v.sum(numbers);
-console.log(sum);
+// Function Declaration
+function add(a, b) {
+    return a + b;
+}
+
+// Function Expression
+const multiply = function(a, b) {
+  return a * b;
+}
+
+const subtract=(a,b)=>{
+    return a-b;
+}
+const square=x=>x*x;
+greet=()=>console.log("hello");
+let _ = multiply(6, 9);
+console.log(_);
+_ = square(6);
+console.log(_);
+greet();
